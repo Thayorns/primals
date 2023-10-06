63f7894a361aba5070590608b90b37defff06f3f
Thayorn <thayornSwordsMan@gmail.com>
2023-10-06 17:20:05 +0300

    some boxshadow

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -20,6 +20,17 @@ function App() {
   const crossPathA = useTransform(x, [-10, -55], [0, 1]);
   const crossPathB = useTransform(x, [-50, -100], [0, 1]);
   
+  // let primes = [];
+  // for (let i = 2; i <= num; i++) {
+  //   if (primes.every((prime) => i % prime !== 0))
+  //     primes.push(i);
+  // }
+  // return primes.reduce((sum, prime) => sum + prime, 0);let primes = [];
+  // for (let i = 2; i <= num; i++) {
+  //   if (primes.every((prime) => i % prime !== 0))
+  //     primes.push(i);
+  // }
+  // return primes.reduce((sum, prime) => sum + prime, 0);
  
   
   return (
