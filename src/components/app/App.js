409c4bd322e8675e5bf1c37e5fba34a6c2771b76
Thayorn <thayornSwordsMan@gmail.com>
2023-10-14 22:35:56 +0300

    functionallity

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,11 +1,12 @@
-import { motion, useMotionValue, useTransform } from "framer-motion"
-import { Input, Button } from 'antd';
+import { motion, useMotionValue, useTransform, animate } from "framer-motion"
+import { Input, Button, Popover } from 'antd';
+import { useEffect } from 'react'
 
 import './App.css';
-import { useState} from "react";
+import { useState, useMemo} from "react";
 
 function App() {
-  const x = useMotionValue(0);
+  const x = useMotionValue(0)
   const xInput = [-60, 0, 60];
   const background = useTransform(x, xInput, [
     "linear-gradient(180deg, #ff008c 0%, rgb(211, 9, 225) 100%)",
@@ -20,22 +21,23 @@ function App() {
   const tickPath = useTransform(x, [10, 100], [0, 1]);
   const crossPathA = useTransform(x, [-10, -55], [0, 1]);
   const crossPathB = useTransform(x, [-50, -100], [0, 1]);
-  const [userNum, setUserNum] = useState()
-  // const inputRef = useRef(null)
+  const [userNum, setUserNum] = useState(null)
+  
+  
+  // const text = <span>Congratulations! You did it!</span>;//POPOVER
+  // const [showArrow, setShowArrow] = useState(true);
+  // const [arrowAtCenter, setArrowAtCenter] = useState(false)
+  // const mergedArrow = useMemo(() => {
+  //   if (arrowAtCenter) return { pointAtCenter: true };
+  //   return showArrow;
+  // }, [showArrow, arrowAtCenter])
+  //--------------------------------------
   
   const primeCheck = (userNum) => {
-    const redOne = document.querySelector('.red-one')
-    const redTwo = document.querySelector('.red-two')
-    const green = document.querySelector('.green')
-    const progressIcon = document.querySelector('path.progress-icon')
-    const container = document.querySelector('.container')
-    const box = document.querySelector('.box')
-  
     let isPrime = true
     if (userNum <= 1 || userNum == null) {
       isPrime = false
-    }
-    
+    }    
     for (let i = 2; i <= Math.sqrt(userNum); i++) {
       if (userNum % i === 0) {
         isPrime = false
@@ -43,40 +45,15 @@ function App() {
       }
     }
     if (isPrime) {
-      // clear previous
-      progressIcon.classList.remove('not-prime-color')
-      redOne.classList.remove('red-stroke')
-      redTwo.classList.remove('red-stroke')
-      // --------------------------------------------//
-      // add new
-      box.style.cssText = `
-        transform: translateX(100%);
-        transition: all ease 0.5s;
-      `
-      container.style.cssText = `
-        background: linear-gradient(180deg, rgb(230, 255, 0) 0%, rgb(3, 209, 0) 100%);
-      `
-      progressIcon.classList.add('prime-color')
-      green.classList.add('green-stroke')
+      animate(x, 100, { duration: 0.9 })
     } else {
-      // clear previous
-      progressIcon.classList.remove('prime-color')
-      green.classList.remove('green-stroke')
-      // --------------------------------------------//
-      // add new
-      box.style.cssText = `
-        transform:translateX(-100%);
-        transition: all ease 0.5s;
-      `
-      container.style.cssText = `
-        background: linear-gradient(180deg, #ff008c 0%, rgb(211, 9, 225) 100%);
-      `
-      progressIcon.classList.add('not-prime-color')
-      redOne.classList.add('red-stroke')
-      redTwo.classList.add('red-stroke')
+      animate(x, -100, { duration: 0.9 })
     }
   }
- 
+  useEffect(() => {
+    primeCheck(userNum)
+    animate(x, 0, { duration: 0.5 })
+  }, [userNum])
   
   return (
     <div className="App">
@@ -90,7 +67,6 @@ function App() {
         <Input placeholder="your number"
           required
           type="number"
-          // ref={inputRef}
           onChange={(e)=> setUserNum(e.target.value)}
           />
         <Button 
@@ -101,47 +77,50 @@ function App() {
           Click!
         </Button>
         
-        
-        <motion.div className="box" 
-          style={{ x }} 
-          drag="x" 
-          dragConstraints={{ left: 0, right: 0}}>
-          
-          <svg className="progress-icon" viewBox="0 0 50 50">
-            <motion.path className='progress-icon'// ALL stuff
-              fill="none"
-              strokeWidth="2"
-              stroke={color}
-              d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
-              // style={{ translateX: 5, translateY: 5 }} он теперь в CSS
-            />
-            <motion.path className='green'
-              fill="none"
-              strokeWidth="2"
-              stroke={color}
-              d="M14,26 L 22,33 L 35,16"
-              strokeDasharray="0 1"
-              style={{ pathLength: tickPath }}
-            />
-            <motion.path className='red-one'// RED LEFT
-              fill="none"
-              strokeWidth="2"
-              stroke={color}
-              d="M17,17 L33,33"
-              strokeDasharray="0 1"
-              style={{ pathLength: crossPathA }}
-            />
-            <motion.path className='red-two'// RED RIGHT
-              fill="none"
-              strokeWidth="2"
-              stroke={color}
-              d="M33,17 L17,33"
-              strokeDasharray="0 1"
-              style={{ pathLength: crossPathB }}
-            />
-          </svg>
-        </motion.div>
-        
+        {/* <Popover placement="leftBottom" title={text} arrow={mergedArrow}></Popover> */}
+        <div className="switch">
+          <motion.div className="box" 
+            style={{ x }}
+            animate={{ opacity: 1 }}
+            initial={{ opacity: 0 }}
+            drag="x" 
+            dragConstraints={{ left: 0, right: 0}}>
+            
+            <svg className="progress-icon" viewBox="0 0 50 50">
+              <motion.path className='progress-icon'// ALL stuff
+                fill="none"
+                strokeWidth="2"
+                stroke={color}
+                d="M 0, 20 a 20, 20 0 1,0 40,0 a 20, 20 0 1,0 -40,0"
+                // style={{ translateX: 5, translateY: 5 }} он теперь в CSS
+              />
+              <motion.path className='green'
+                fill="none"
+                strokeWidth="2"
+                stroke={color}
+                d="M14,26 L 22,33 L 35,16"
+                strokeDasharray="0 1"
+                style={{ pathLength: tickPath }}
+              />
+              <motion.path className='red-one'// RED LEFT
+                fill="none"
+                strokeWidth="2"
+                stroke={color}
+                d="M17,17 L33,33"
+                strokeDasharray="0 1"
+                style={{ pathLength: crossPathA }}
+              />
+              <motion.path className='red-two'// RED RIGHT
+                fill="none"
+                strokeWidth="2"
+                stroke={color}
+                d="M33,17 L17,33"
+                strokeDasharray="0 1"
+                style={{ pathLength: crossPathB }}
+              />
+            </svg>
+          </motion.div>
+        </div>
         
       </motion.div>
     </div>
